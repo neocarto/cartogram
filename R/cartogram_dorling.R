b43dbb32518bfcad678135ae299098692a1f75b2
Nicolas Lambert <nicolas.lambert@cnrs.fr>
2020-01-29 14:46:43 +0100

    dorling cartogram legend

--- R/cartogram_dorling.R
+++ R/cartogram_dorling.R
@@ -5,6 +5,8 @@
 #' @param x SpatialPolygonsDataFrame, SpatialPointsDataFrame or an sf object
 #' @param weight Name of the weighting variable in x
 #' @param k Share of the bounding box of x filled by the larger circle
+#' @param l Multiplying factor (in map units) to adjust the size of the circles.
+#' Using l instead of k allows the cartograms to be made comparable with each other.
 #' @param m_weight Circles' movements weights. An optional vector of numeric weights 
 #' (0 to 1 inclusive) to 
 #' apply to the distance each circle moves during pair-repulsion. A weight of 0 
@@ -17,38 +19,25 @@
 #' @export
 #' @references Dorling, D. (1996). Area Cartograms: Their Use and Creation. In Concepts and Techniques in Modern Geography (CATMOG), 59.
 #' @examples
-#' library(maptools)
-#' library(cartogram)
-#' library(rgdal)
-#' data(wrld_simpl)
-#' 
-#' # Remove uninhabited regions
-#' afr <- spTransform(wrld_simpl[wrld_simpl$REGION==2 & wrld_simpl$POP2005 > 0,], 
-#'                    CRS("+init=epsg:3395"))
-#' 
-#' # Create cartogram
-#' afr_carto <- cartogram_dorling(afr, "POP2005")
-#' 
-#' # Plot 
-#' par(mfcol=c(1,2))
-#' plot(afr, main="original")
-#' 
-#' plot(afr, main="distorted (sp)")
-#' plot(afr_carto, col = "red", add=TRUE)
-#' 
-#' # Same with sf objects
+# Packages
 #' library(sf)
+#' library(rnaturalearth)
+#' library(cartogram)
 #' 
-#' afr_sf = st_as_sf(afr)
+#' # Data inport and Handling
+#' world <- ne_countries(returnclass = "sf")
+#' crs <- "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+#' world <- st_transform(world, crs = crs)
+#' world <- world[!is.na(world$pop_est),]
 #' 
-#' afr_sf_carto <- cartogram_dorling(afr_sf, "POP2005")
+#' # Cartogram building
+#' pop.dorling <- cartogram_dorling(world, "pop_est")
+#' plot(st_geometry(pop.dorling), col ="red", border="white")
 #' 
-#' # Plot 
-#' par(mfcol=c(1,3))
-#' plot(afr, main="original")
-#' plot(afr_carto, main="distorted (sp)")
-#' plot(st_geometry(afr_sf_carto), main="distorted (sf)")
-cartogram_dorling <- function(x, weight, k = 5, m_weight = 1, itermax= 1000) {
+#' # Layout and Legend
+#' title("World Population")
+
+cartogram_dorling <- function(x, weight, k = 5, l = NULL, m_weight = 1, itermax= 1000) {
   UseMethod("cartogram_dorling")
 }
 
@@ -56,7 +45,7 @@ cartogram_dorling <- function(x, weight, k = 5, m_weight = 1, itermax= 1000) {
 #' @importFrom sf st_is_longlat st_as_sf st_geometry st_coordinates st_geometry st_centroid st_crs
 #' @importFrom packcircles circleRepelLayout
 #' @export
-cartogram_dorling.sf <- function(x, weight, k = 5, m_weight = 1, itermax= 1000){
+cartogram_dorling.sf <- function(x, weight, k = 5, l = NULL, m_weight = 1, itermax= 1000){
   # proj or unproj
   if (sf::st_is_longlat(x)) {
     stop('Using an unprojected map. This function does not give correct centroids and distances for longitude/latitude data:\nUse "st_transform()" to transform coordinates to another projection.', call. = F)
@@ -67,7 +56,13 @@ cartogram_dorling.sf <- function(x, weight, k = 5, m_weight = 1, itermax= 1000){
   dat.init <- data.frame(sf::st_coordinates(sf::st_centroid(sf::st_geometry(x))),
                          v = x[[weight]])
   surf <- (max(dat.init[,1]) - min(dat.init[,1])) *  (max(dat.init[,2]) - min(dat.init[,2]))
-  dat.init$v <- dat.init$v * (surf * k / 100) / max(dat.init$v)
+  # comparable or not
+  if(!is.null(l) == TRUE){
+    dat.init$v <- dat.init$v * l
+  }
+  if(!is.null(k) == TRUE){
+    dat.init$v <- dat.init$v * (surf * k / 100) / max(dat.init$v)
+  }
   # circles layout and radiuses
   res <- packcircles::circleRepelLayout(x = dat.init, xysizecols = 1:3,
                                         wrap = FALSE, sizetype = "area",
@@ -83,6 +78,91 @@ cartogram_dorling.sf <- function(x, weight, k = 5, m_weight = 1, itermax= 1000){
 
 #' @rdname cartogram_dorling
 #' @export
-cartogram_dorling.SpatialPolygonsDataFrame <- function(x, weight, k = 5, m_weight = 1, itermax = 1000){
+cartogram_dorling.SpatialPolygonsDataFrame <- function(x, weight, k = 5, l = NULL, m_weight = 1, itermax = 1000){
   as(cartogram_dorling.sf(sf::st_as_sf(x), weight = weight, k = k, m_weight = m_weight, itermax = itermax), "Spatial")
+}
+
+
+#' @title Dorling Cartogram Legend
+#' @description Build and display the legend of a Dorling cartogram
+#' @name cartogram_dorling_legend
+#' @param x dorling cartogram generetd with the function cartogram_dorling(). sf object.
+#' @param var name of the vector containing the data 
+#' @param pos position of the legend. a vector of two coordinates in map units (c(x, y)
+#' @param col fill color.
+#' @param border stroke color.
+#' @param values.cex size of the values in the legend.
+#' @param values.round rounding of numbers
+#' @param lty line style
+#' @param nb.circles number of circles in the legend
+#' @param title.txt title of the legend.
+#' @param title.cex size of the legend title.
+#' @param title.font font of the legend title.
+#' @export
+#' @references Dorling, D. (1996). Area Cartograms: Their Use and Creation. In Concepts and Techniques in Modern Geography (CATMOG), 59.
+#' @examples
+# Packages
+#' library(sf)
+#' library(rnaturalearth)
+#' library(cartogram)
+#' 
+#' # Data inport and Handling
+#' world <- ne_countries(returnclass = "sf")
+#' crs <- "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+#' world <- st_transform(world, crs = crs)
+#' world <- world[!is.na(world$pop_est),]
+#' 
+#' # Cartogram building
+#' pop.dorling <- cartogram_dorling(world, "pop_est")
+#' plot(st_geometry(pop.dorling), col ="red", border="white")
+#' 
+#' # Layout and Legend
+#' title("World Population")
+#' cartogram_dorling_legend(x = pop.dorling, pos <- NULL, var = "pop_est", col = "red",
+#'                          border = "black", lwd = 1, values.cex = 0.6, values.round = 0,
+#'                          lty = 3, nb.circles =  4, title.txt = "Number of inhabitants.",
+#'                         title.cex = 0.8, title.font = 2)
+cartogram_dorling_legend <- function(x, var, pos = NULL, col = "white",
+                                     border = "black",lwd = 1, values.cex = 0.6,
+                                     values.round = 0, lty = 3, nb.circles = 4,
+                                     title.txt = "Title of the legend",
+                                     title.cex = 0.8,
+                                     title.font = 2) {
+  
+  # Radii & Values
+  v <- x
+  st_geometry(v) <- NULL
+  v <- v[,var]
+  r <- sqrt(as.numeric(st_area(x))/pi)
+  radii <- seq(from = max(r), to = min(r), length.out = nb.circles)
+  sle <- radii * radii * pi
+  values <- sle * max(v) / sle[1]
+  
+  # Positions
+  par()$usr
+  
+  delta <- (par()$usr[2] - par()$usr[1]) / 50
+  if(length(pos) != 2){
+    pos <- c(par()$usr[1] + radii[1] + delta,par()$usr[3] + delta)
+  }
+  
+  # Circles
+  
+  for(i in 1:nb.circles){
+    # circles
+    posx <- pos[1]
+    posy <- pos[2] + radii[i]
+    p <- st_sfc(st_point(c(posx,posy)))
+    circle <- st_buffer(st_as_sf(p), dist = radii[i])
+    plot(circle, col = col, border = border, lwd=lwd, add=T)
+    # lines
+    segments(posx, posy + radii[i], posx + radii[1] + radii[1]/10, col = border, lwd=lwd, lty = lty)
+    # texts
+    text(x = posx + radii[1] + radii[1]/5, y = posy + radii[i], 
+         labels = formatC(round(values[i],values.round), big.mark = " ", format = "fg", digits = values.round), adj = c(0,0.5), cex = values.cex)
+  }
+  
+  # Title
+  text(x = posx - radii[1] ,y = posy + radii[1]*2 + radii[1]/3, title.txt,
+       adj = c(0,0), cex = title.cex, font = title.font)
 }
